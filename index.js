cbeaca1505afe5caf9ca5bc3e4b793f88479aec5
pbedrosian <peter_bedrosian@yahoo.com>
2020-09-05 17:38:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your functions here
+function map(arr, func) {
+    return arr.map(element => func(element))
+}
+
+function reduce(arr, func, start) {
+    let val = (!!start) ? start : arr[0];
+    let i = (!!start) ? 0 : 1;
+
+    for (; i < arr.length; i++) {
+        val = func(arr[i], val)
+    }
+
+    return val;
+}
